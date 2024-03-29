30aef119427c20456a3bc73bbd67d02a441aae42
SoundharNanthakumar <165360962+SoundharNanthakumar@users.noreply.github.com>
2024-03-29 12:11:43 +0530

    datatype

---
+++ Dataypes.java
@@ -0,0 +1,42 @@
+class dt{	
+	boolean g; //--------boolean value neither true or false-----------------------
+	
+public static void main(String[]args){
+	//-------------------premethive Data Types la number storages-------------------
+	
+	byte a=127;
+	System.out.println(a);
+	
+	short b=300;
+	System.out.println(b);
+	
+	int c=4000;
+	System.out.println(c);
+	
+	long d=67598598989l ;
+	System.out.println(d);
+	
+	//-------------------premethive Data Types la decimal number storages------------
+	
+	float e=10.99f;
+	System.out.println(e);
+	
+	double f=120.989809;
+	System.out.println(f);
+	
+    //-------------------premethive Data Types la boolean----------------------------
+
+    dt doo=new dt();
+	System.out.println(doo.g);
+	
+	//-------------------premethive Data Types la single letter (charater)------------
+    char h='S';
+	System.out.println(h);
+	
+	//-------------------Nonpremetive datatype la String------------------------------
+	
+	String i="Surya";
+	System.out.println(i);
+	
+}
+}
